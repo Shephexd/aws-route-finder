8c1301b5926cfdca79e20639952b45485a3724d7
Taekyung Han <taekyung@amazon.com>
2024-07-17 23:00:24 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", encoding="utf-8") as f:
+    requirements = []
+    for _requirement in f.read().splitlines():
+        requirements.append(_requirement)
+
+
+setuptools.setup(
     name='routefinder',
     version='0.1.0',
     author="Taekyung Han",
     author_email="taekyung@amazon.com",
     description="AWS Network Reachability Test Tool",
     license="Apache 2.0 License",
-    packages=find_packages(include=['routefinder'])
+    packages=setuptools.find_packages(include=['routefinder']),
+    install_requires=requirements
 )
